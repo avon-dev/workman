b0a4414da68807dfdd317aa681bf96041880a15d
tlsgks48 <tlsgks486@gmail.com>
2020-01-03 17:30:48 +0900

    layout

---
+++ app/src/main/java/com/example/workman/ScheduleListSet.kt
@@ -0,0 +1,12 @@
+package com.example.workman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleListSet : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_list_set)
+    }
+}
