6c475ee816b7d95e7a14ef5c31f950e9eb676f82
leejusang <toy2437@naver.com>
2020-01-05 13:53:38 +0900

    jusang

---
+++ app/src/main/java/com/example/workman/StartPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.workman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_page)
+    }
+}
