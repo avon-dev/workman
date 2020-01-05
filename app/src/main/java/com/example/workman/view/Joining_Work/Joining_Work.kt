2ede153a94964438f371c26822a2e7db9e44507d
rain2353 <vvkkvv@naver.com>
2020-01-05 14:26:16 +0900

    hyun_se. 직장 합류하기

---
+++ app/src/main/java/com/example/workman/view/Joining_Work/Joining_Work.kt
@@ -0,0 +1,13 @@
+package com.example.workman.view.Joining_Work
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workman.R
+
+class Joining_Work : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_joining__work)
+    }
+}
