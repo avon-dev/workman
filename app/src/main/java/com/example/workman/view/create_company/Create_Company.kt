d20bd3f7af0dfbc1fb97c80366d19c521a7e4d28
rain2353 <vvkkvv@naver.com>
2020-01-05 13:48:30 +0900

    hyun_se. select_company, create_company

---
+++ app/src/main/java/com/example/workman/view/create_company/Create_Company.kt
@@ -0,0 +1,13 @@
+package com.example.workman.view.create_company
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workman.R
+
+class Create_Company : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create__company)
+    }
+}
---
+++ app/src/main/java/com/example/workman/view/select_company/Select_company.kt
@@ -0,0 +1,13 @@
+package com.example.workman.view.select_company
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workman.R
+
+class Select_company : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_company)
+    }
+}
