0963890a8cecb9b1c585d03f58cc48e35c7cfe03
rain2353 <vvkkvv@naver.com>
2020-01-05 14:25:49 +0900

    hyun_se. 직장 위치 추가하기

---
+++ app/src/main/java/com/example/workman/view/create_company/Add_Work_Location.kt
@@ -0,0 +1,13 @@
+package com.example.workman.view.create_company
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workman.R
+
+class Add_Work_Location : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add__work__location)
+    }
+}
