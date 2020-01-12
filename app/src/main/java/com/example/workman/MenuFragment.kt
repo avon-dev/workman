bab3564ed359f92cd09c03a3031ee950498dab74
yeonjun <44648608+fxkingcode@users.noreply.github.com>
2020-01-12 13:19:24 +0900

    Menu

--- app/src/main/java/com/example/workman/MenuFragment.kt
+++ app/src/main/java/com/example/workman/MenuFragment.kt
@@ -1,11 +1,14 @@
 package com.example.workman
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_menu.*
+import kotlinx.android.synthetic.main.fragment_menu.view.*
 
 /**
  * 더보기
@@ -17,8 +20,29 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_menu, container, false)
+
+        rootView.myprofilebutton.setOnClickListener {
+            val nextIntent = Intent(activity, MyprofileActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+        rootView.companyprofilebutton.setOnClickListener {
+            val nextIntent = Intent(activity, CompanyprofileActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+        rootView.employeebutton.setOnClickListener {
+            val nextIntent = Intent(activity, EmployeeActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+        rootView.vacationbutton.setOnClickListener {
+            val nextIntent = Intent(activity, VacationActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+
+        return rootView
     }
-
-
 }
